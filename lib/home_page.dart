906c399c63c9bd0632968b2d80909a9f140b250c
Harshil Shah <sharshil1299@gmail.com>
2019-09-16 15:23:32 +0530

    stateful Listview
    Remaining Refersh

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:classroom_flutter/MyListView.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
@@ -9,9 +10,6 @@ import 'package:file_picker/file_picker.dart';
 import 'package:amazon_cognito_identity_dart/cognito.dart';
 import 'package:amazon_cognito_identity_dart/sig_v4.dart';
 import 'package:path/path.dart';
-import 'package:xml/xml.dart' as xml;
-
-
 
 import './policy.dart';
 
@@ -57,141 +55,13 @@ class HomePage extends StatelessWidget {
     return credentials;
   }
 
-  Future cognitologintest() async {
-    var credentials = await getCredentials();
-    print("Access key id: " + credentials.accessKeyId);
-    print("Secret Access Key: " + credentials.secretAccessKey);
-    print("Session Token: " + credentials.sessionToken);
-  }
+//  Future cognitologintest() async {
+//    var credentials = await getCredentials();
+//    print("Access key id: " + credentials.accessKeyId);
+//    print("Secret Access Key: " + credentials.secretAccessKey);
+//    print("Session Token: " + credentials.sessionToken);
+//  }
 
-  Future<http.Response> getFileHelper(String S3Key) async {
-    var credentials = await getCredentials();
-
-    final host = 's3.amazonaws.com';
-    final region = _region;
-    final service = 's3';
-    final key = bucketname + '/' + S3Key;
-
-    final payload = SigV4.hashCanonicalRequest('');
-    final datetime = SigV4.generateDatetime();
-    final canonicalRequest = '''GET
-${'/$key'.split('/').map((s) => Uri.encodeComponent(s)).join('/')}
-
-host:$host
-x-amz-content-sha256:$payload
-x-amz-date:$datetime
-x-amz-security-token:${credentials.sessionToken}
-
-host;x-amz-content-sha256;x-amz-date;x-amz-security-token
-$payload''';
-    final credentialScope =
-    SigV4.buildCredentialScope(datetime, region, service);
-    final stringToSign = SigV4.buildStringToSign(datetime, credentialScope,
-        SigV4.hashCanonicalRequest(canonicalRequest));
-    final signingKey = SigV4.calculateSigningKey(
-        credentials.secretAccessKey, datetime, region, service);
-    final signature = SigV4.calculateSignature(signingKey, stringToSign);
-
-    final authorization = [
-      'AWS4-HMAC-SHA256 Credential=${credentials.accessKeyId}/$credentialScope',
-      'SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token',
-      'Signature=$signature',
-    ].join(',');
-
-    final uri = Uri.https(host, key);
-    http.Response response;
-    try {
-      response = await http.get(uri, headers: {
-        'Authorization': authorization,
-        'x-amz-content-sha256': payload,
-        'x-amz-date': datetime,
-        'x-amz-security-token': credentials.sessionToken,
-      });
-    } catch (e) {
-      print(e);
-    }
-    return response;
-  }
-  Future<http.Response> deleteFileHelper(String S3Key) async {
-    var credentials = await getCredentials();
-
-    final host = 's3.amazonaws.com';
-    final region = _region;
-    final service = 's3';
-    final key = bucketname + '/' + S3Key;
-
-    final payload = SigV4.hashCanonicalRequest('');
-    final datetime = SigV4.generateDatetime();
-    final canonicalRequest = '''DELETE
-${'/$key'.split('/').map((s) => Uri.encodeComponent(s)).join('/')}
-
-host:$host
-x-amz-content-sha256:$payload
-x-amz-date:$datetime
-x-amz-security-token:${credentials.sessionToken}
-
-host;x-amz-content-sha256;x-amz-date;x-amz-security-token
-$payload''';
-    final credentialScope =
-    SigV4.buildCredentialScope(datetime, region, service);
-    final stringToSign = SigV4.buildStringToSign(datetime, credentialScope,
-        SigV4.hashCanonicalRequest(canonicalRequest));
-    final signingKey = SigV4.calculateSigningKey(
-        credentials.secretAccessKey, datetime, region, service);
-    final signature = SigV4.calculateSignature(signingKey, stringToSign);
-
-    final authorization = [
-      'AWS4-HMAC-SHA256 Credential=${credentials.accessKeyId}/$credentialScope',
-      'SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token',
-      'Signature=$signature',
-    ].join(',');
-
-    final uri = Uri.https(host, key);
-    http.Response response;
-    try {
-      response = await http.delete(uri, headers: {
-        'Authorization': authorization,
-        'x-amz-content-sha256': payload,
-        'x-amz-date': datetime,
-        'x-amz-security-token': credentials.sessionToken,
-      });
-    } catch (e) {
-      print(e);
-    }
-    return response;
-  }
-
-  Future<List> getFileNames() async {
-    http.Response aa = await getFileHelper("");
-    String xx = aa.body;
-    print(xx);
-
-    var document = xml.parse(xx);
-    print(document.children.length);
-
-    var files = document.findAllElements('Key');
-
-    List fileList = new List<String>();
-
-    for (int i = 0; i < files.length; i++) {
-      var file = files.elementAt(i);
-      print(file.text);
-      fileList.add(file.text);
-    }
-    print(fileList);
-
-    return fileList;
-  }
-
-  Future getFile(String fileName) async {
-    http.Response aa = await getFileHelper(fileName);
-    print(aa.body);
-  }
-
-  Future delFileDemo(String fileName) async {
-    http.Response aa = await deleteFileHelper(fileName);
-    print(aa.body);
-  }
 
   Future uploadFileToS3(String pathString) async {
     var credentials = await getCredentials();
@@ -269,19 +139,10 @@ $payload''';
         uploadFileToS3(path);
       }
     }
-    List data = [1, 2,23,4];
 
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Share and Care'),
-          actions: <Widget>[
-      // action button
-      IconButton(
-      icon: Icon(Icons.refresh),
-      onPressed: () {
-       getFileNames();
-      },
-    ),]
       ),
 //      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       body:
@@ -290,28 +151,18 @@ $payload''';
         crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-          new FutureBuilder<List>(
-            future: getFileNames(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) return Container();
-              List<String> posts = snapshot.data;
-              return new Flexible(child: new ListView.builder(
-                  itemCount: posts.length,
-                  itemBuilder: (BuildContext ctxt, int index) {
-                    return new ListTile(
-                        title: Text(posts[index]),
-                        trailing:Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              IconButton(icon: Icon(Icons.cloud_download), onPressed: () {getFile(posts[index]);}),
-                              IconButton(icon: Icon(Icons.delete_forever), onPressed: () {delFileDemo(posts[index]);})
-                            ]),
-                    );
-                  }
-              ),);
+         new MyListView(username: username, password: password),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: new FloatingActionButton.extended(
+                  elevation: 10.0,
+                  icon: Icon(Icons.add),
+                  label: Text("Add Files"),
+                  onPressed: fileSelector,
 
-            },
-          ),
+                ),
+              )
+
 //              new RaisedButton(
 //                child: new Text('Cognito Login Test'),
 //                onPressed: cognitologintest,
@@ -320,13 +171,7 @@ $payload''';
 //            child: new Text('GetFileList'),
 //            onPressed: getFileNames,
 //          ),
-          new FloatingActionButton.extended(
-            elevation: 20.0,
-            icon: Icon(Icons.add),
-            label: Text("Add Files"),
-            onPressed: fileSelector,
 
-          ),
 
 
 
